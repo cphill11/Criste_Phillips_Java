1dac44367f9dfeb567e4d21e7a5f1ee70663297c
Criste Phillips <criste.phillips@yahoo.com>
2022-10-06 14:01:58 -0400

    JUnit testing added

--- M1-Challenge-Phillips-Criste/CalculatorObject/src/test/java/com/company/CalculatorTest.java
+++ M1-Challenge-Phillips-Criste/CalculatorObject/src/test/java/com/company/CalculatorTest.java
@@ -1,51 +1,80 @@
-//package com.company;
-//
-//import org.junit.Test;
-//
-//import static org.junit.Assert.assertEquals;
-//
-//    public class CalculatorTest {
-//
-//        private Calculator calc = new Calculator();
-//
-//        @Test
-//        public void shouldReturnSumOfTwoIntegersWhenGivenTwoIntegers() {
-//
-//            String failMessage = "Expected calculator to handle integer addition.";
-//
-//            assertEquals(failMessage, 5, calc.add(2, 3));
-//            assertEquals(failMessage, 17, calc.add(20, -3));
-//            assertEquals(failMessage, 1, calc.add(0, 1));
-//        }
-//
-//        @Test
-//        public void shouldReturnDifferenceOfTwoIntegersWhenGivenTwoIntegers() {
-//
-//            String failMessage = "Expected calculator to handle integer subtraction.";
-//
-//            assertEquals(failMessage, -1, calc.subtract(2, 3));
-//            assertEquals(failMessage,  23, calc.subtract(20, -3));
-//            assertEquals(failMessage, 10, calc.subtract(11, 1));
-//        }
-//
-//        @Test
-//        public void shouldReturnProductOfTwoIntegersWhenGivenTwoIntegers() {
-//
-//            String failMessage = "Expected calculator to handle integer multiplication.";
-//
-//            assertEquals(failMessage, 6, calc.multiply(2, 3));
-//            assertEquals(failMessage, -60, calc.multiply(20, -3));
-//            assertEquals(failMessage, 0, calc.multiply(0, 1));
-//        }
-//
-//        @Test
-//        public void shouldReturnQuotientOfTwoIntegersWhenGivenTwoIntegers() {
-//
-//            String failMessage = "Expected calculator to handle integer division.";
-//
-//            assertEquals(failMessage, 1, calc.divide(4, 3));
-//            assertEquals(failMessage, -5, calc.divide(20, -4));
-//            assertEquals(failMessage, 3, calc.divide(18, 5));
-//        }
-//    }
-//}
+package com.company;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+    public class CalculatorTest {
+
+        @Test
+        public void add() {
+            Calculator calc = new Calculator();
+            int actualResult = calc.add(1,2);
+            int expectedResult = 3;
+            assertEquals(expectedResult, actualResult);
+        }
+
+        @Test
+        public void subtract() {
+            Calculator calc = new Calculator();
+            int actualResult = calc.subtract(23,52);
+            int expectedResult = -29;
+            assertEquals(expectedResult, actualResult);
+        }
+
+        @Test
+        public void multiply() {
+            Calculator calc = new Calculator();
+            int actualResult = calc.multiply(34,2);
+            int expectedResult = 68;
+            assertEquals(expectedResult, actualResult);
+        }
+        @Test
+        public void divideEvenly() {
+            Calculator calc = new Calculator();
+            int actualResult = calc.divide(12,3);
+            int expectedResult = 4;
+            assertEquals(expectedResult, actualResult);
+        }
+
+        @Test
+        public void divideOddly() {
+            Calculator calc = new Calculator();
+            int actualResult = calc.divide(12,7);
+            int expectedResult = 1;
+            assertEquals(expectedResult, actualResult);
+        }
+
+        @Test
+        public void addDouble() {
+            Calculator calc = new Calculator();
+            double actualResult = calc.add(3.4, 2.3);
+            double expectedResult = 5.7;
+            assertEquals(expectedResult, actualResult, 1E-8);
+        }
+
+        @Test
+        public void subtractDouble() {
+            Calculator calc = new Calculator();
+            double actualResult = calc.subtract(5.5,0.5);
+            double expectedResult = 5;
+            assertEquals(expectedResult, actualResult, 1E-8);
+        }
+
+        @Test
+        public void multiplyDouble() {
+            Calculator calc = new Calculator();
+            double actualResult = calc.multiply(6.7,4.4);
+            double expectedResult = 29.48;
+            assertEquals(expectedResult, actualResult, 1E-8);
+        }
+        @Test
+        public void divideDouble() {
+            Calculator calc = new Calculator();
+            float actualResult = calc.divide(12,3);
+            float expectedResult = 4;
+            assertEquals(expectedResult, actualResult, 1E-8);
+        }
+
+
+}
