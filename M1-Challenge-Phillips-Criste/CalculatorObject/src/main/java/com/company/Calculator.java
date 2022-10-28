64387b3e134976e582c82f467f878e38e61096ac
Criste Phillips <criste.phillips@yahoo.com>
2022-10-28 14:53:29 -0400

    initial push of starter code

--- M1-Challenge-Phillips-Criste/CalculatorObject/src/main/java/com/company/Calculator.java
+++ M1-Challenge-Phillips-Criste/CalculatorObject/src/main/java/com/company/Calculator.java
@@ -4,10 +4,13 @@ package com.company;
 public class Calculator {
 
 
-    public int add(int a, int b) { return a + b; }
+    public int add(int a, int b) {
+        return a + b; }
 
-    public double add(double a, double b) { return a + b; }
+    public double add(double a, double b) {
+        return a + b; }
     public int subtract(int a, int b) {
+
         return a - b;
     }
 
