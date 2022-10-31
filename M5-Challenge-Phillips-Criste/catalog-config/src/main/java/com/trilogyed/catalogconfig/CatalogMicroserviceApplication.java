dae98152317fbfc1c4cfdd79de0566d6299b3044
Criste Phillips <criste.phillips@yahoo.com>
2022-10-31 13:57:48 -0400

    initial schema and data push

---
+++ M5-Challenge-Phillips-Criste/catalog-config/src/main/java/com/trilogyed/catalogconfig/CatalogMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.trilogyed.catalogconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogMicroserviceApplication.class, args);
+	}
+
+}
