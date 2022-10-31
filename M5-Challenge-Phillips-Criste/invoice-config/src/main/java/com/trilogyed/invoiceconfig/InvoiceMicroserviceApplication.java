eb81788d03c41dd20fcb872429d6fc64d1e91354
Criste Phillips <criste.phillips@yahoo.com>
2022-10-31 13:56:32 -0400

    data and schema initial push

---
+++ M5-Challenge-Phillips-Criste/invoice-config/src/main/java/com/trilogyed/invoiceconfig/InvoiceMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.trilogyed.invoiceconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceMicroserviceApplication.class, args);
+	}
+
+}
---
+++ M5-Challenge-Phillips-Criste/invoice-config/src/test/java/com/trilogyed/invoiceconfig/InvoiceMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trilogyed.invoiceconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
